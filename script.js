e63bc8f43379187156860cc7d59ac4b3a5978e94
Cody Hoover <codydevloop@gmail.com>
2020-04-12 07:05:16 -0700

    added code for spoonacular

--- script.js
+++ script.js
@@ -7,75 +7,52 @@ $(document).ready(function () {
     $("#searchBtn").on("click", function () {
 
         let searchFood = $("#foodInput").val();
-        searchEdamam(searchFood);
+        searchSpoonacular(searchFood);
 
 
         searchOpenWeather(zip, function (lat, lon) {
             // console.log("lat :" + lat + " long :" + lon);
-            searchZomato(lat,lon, searchFood);
+            // searchZomato(lat,lon, searchFood);
 
-        });
-
-        // searchEdamamFoodDB(searchFood);
+        })
         
 
     });
 
 
-    // function searchEdamamFoodDB(usersRecipe) {
 
-        // **********************************************************************************
-        //  Make FoodDB seach call to Edamam,  locate cuisine lablel
-        // *********************************************************************************
+    //**********************************************************************************
+    //  Spoonacular - Michelle
+    //********************************************************************************
 
-        //let edamamAppID = "da43e7e75";
-        //let edamamAppKey = "d027a0191dc9b7613941ecea2e2fb5243";
-        //let userSubmittedRecipe = recipe;
+ 
 
-    //     // let edamamRecipeQueryURL = "https://api.edamam.com/search?q=" + searchFood + "&app_id=6ed8e227&app_key=b625542fdcf8fe77f73a1d01bbcbe005";
-
-    //         $.ajax({
-
-    //             url: "https://api.edamam.com/search?q="+ usersRecipe +"&app_id=6ed8e227&app_key=b625542fdcf8fe77f73a1d01bbcbe005",
-    //             method: "GET"
-    //         })
-
-    //             .then(function (edamamResponse) {
-    //                 console.log(response.hits[0].recipe);
-    //                 console.log(response.hits[0].recipe.dietLabels)
-    //                 console.log(response.hits[0].recipe.healthLabels)
-
-    //             });
-    // };
-
-
-
-    function searchEdamam(usersRecipe) {
-
-        //**********************************************************************************
-        //  Make Recipe seach call to Edamam,  locate cuisine lablel
-        //*********************************************************************************
-
-        let edamamAppID = "d9d2f227";
-        let edamamAppKey = "d7ef7e1d47c86463be79d5b8cfe85ceb";
-        // let userSubmittedRecipe = recipe;
-
-        let edamamRecipeQueryURL = "https://api.edamam.com/search?q=" + usersRecipe + "&app_id=" + edamamAppID + "&app_key=" + edamamAppKey;
+    function searchSpoonacular(searchFood){
+        const spoonAPIKey = "e549d6e9b38545a3a604db06b9006011";
+        const spoonQueryURL = 
+        "https://api.spoonacular.com/recipes/random" +
+        // searchfood +
+        "?apiKey=" +
+        spoonAPIKey;
 
         $.ajax({
-
-            url: edamamRecipeQueryURL,
+            url: spoonQueryURL,
             method: "GET"
         })
 
-            .then(function (edamamResponse) {
+            .then(function (spoonResponse) {
 
-                console.log(edamamResponse);
-                // return localcuisine;
+                console.log(spoonResponse);
+            
+
             });
 
-            searchZomato();
     };
+
+
+
+
+
 
     function searchOpenWeather(zipCode, callback) {
 
@@ -139,3 +116,39 @@ $(document).ready(function () {
 
 
 });
+
+
+//**********************************************************************************
+//   OLD CODE - NOT USING
+//*********************************************************************************
+
+    //**********************************************************************************
+    //  Make Recipe seach call to Edamam,  locate cuisine lablel
+    //********************************************************************************
+
+//     function searchEdamam(usersRecipe) {
+
+// *
+
+//         let edamamAppID = "d9d2f227";
+//         let edamamAppKey = "d7ef7e1d47c86463be79d5b8cfe85ceb";
+//         // let userSubmittedRecipe = recipe;
+
+//         let edamamRecipeQueryURL = "https://api.edamam.com/search?q=" + usersRecipe + "&app_id=" + edamamAppID + "&app_key=" + edamamAppKey;
+
+//         $.ajax({
+
+//             url: edamamRecipeQueryURL,
+//             method: "GET"
+//         })
+
+//             .then(function (edamamResponse) {
+
+//                 console.log(edamamResponse);
+//                 // return localcuisine;
+//             });
+
+//             searchZomato();
+//     };
+
+   
