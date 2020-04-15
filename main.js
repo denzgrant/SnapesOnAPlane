483cda3871cbe94b191bd3a6356a08272ff29198
Cody Hoover <codydevloop@gmail.com>
2020-04-15 07:49:25 -0700

    added restraunts to display on main page

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const time = moment().format('MMM Do YYYY, h:mm a');
 const currentHour = moment().format('H');
-console.log(time);
-console.log(currentHour);
+// console.log(time);
+// console.log(currentHour);
 //Current time
 //Im ok with changing font color
 timeAndDate = `<p style="color:blue; padding-top:1em;">It is currently ${time} </p>`
@@ -54,7 +54,9 @@ $(document).ready(function () {
     }
     function recipesDisplay(response) {
         for (var i = 0; i < response.hits.length; i++) {
+           
             var recipesResult = response.hits[i].recipe
+   
 
             var dropDownRecipesNameDisplay = $("<h3>").text(recipesResult.label).attr("data-aos", "flip-left")
 
@@ -102,10 +104,13 @@ $(document).ready(function () {
     // ----------------------------zomato------------------------------
     function zomatoRestaurantCall(latitiude, longitude) {
 
+        // https://developers.zomato.com/api/v2.1/search?count=10&lat=33.34&lon=-111.74
+
         const zomatoApiKey = "8ed1b92667f3ee82f4a77b02be24cf26";
-        const zomatoQueryUrl = "https://developers.zomato.com/api/v2.1/search?q="
+        // const zomatoQueryUrl = "https://developers.zomato.com/api/v2.1/search?q="
+        const zomatoQueryUrl = "https://developers.zomato.com/api/v2.1/search?"
         var searchFood = $("#foodInput").val();
-        const zomatoSearchCount = "&count=10"
+        const zomatoSearchCount = "count=10"
         const zomatoLat = "&lat=" + latitiude
         const zomatoLon = "&lon=" + longitude
         $.ajax({
@@ -113,20 +118,55 @@ $(document).ready(function () {
                 "Accept": "application/json",
                 "user-key": zomatoApiKey
             },
-            url: zomatoQueryUrl + searchFood + zomatoSearchCount + zomatoLat + zomatoLon,
+            url: zomatoQueryUrl + zomatoSearchCount + zomatoLat + zomatoLon,
             // "https://developers.zomato.com/api/v2.1/search?q=burger&count=10&lat=33.427204&lon=-111.939896",
             method: "GET"
         })
 
             .then(function (searchResponse) {
+                for (var i = 0; i < 5; i++){
 
-                console.log(searchResponse);
-                console.log(searchResponse.restaurants)
-                console.log(searchResponse.restaurants[0])
-                console.log(searchResponse.restaurants[0].restaurant.name)
-                console.log(searchResponse.restaurants[0].restaurant.url)
-                console.log(searchResponse.restaurants[0].restaurant.location.address)
-                console.log(searchResponse.restaurants[0].restaurant.highlights)
+                var dropDownRestaurantNameDisplay = $("<h3>").text(searchResponse.restaurants[i].restaurant.name).attr("data-aos", "flip-right")
+               
+                var restaurantUl = $("<ul>");
+                var dropDownRestaurantDisplayLinkLi = $("<li>");
+                var drowDownRestaurantDisplayLink = $("<a>").attr("href", searchResponse.restaurants[i].restaurant.url).text("check the full details");
+                dropDownRestaurantDisplayLinkLi.append(drowDownRestaurantDisplayLink);
+
+                var dropDownRestaurantDisplayLocationLabelsLi = $("<li>").text("Location: " + searchResponse.restaurants[i].restaurant.location.address);
+                var dropDownRestaurantDisplayRatingLabelLi = $("<li>").text("User Rating: " + searchResponse.restaurants[i].restaurant.user_rating.rating_text);
+
+                var dropDownRestaurantDisplayimageLi = $("<li>")
+                var dropDownRestaurantDisplayimage = $("<img>").attr("src", searchResponse.restaurants[i].restaurant.photos[i].photo.thumb_url).attr("alt", searchResponse.restaurants[0].restaurant.name);
+                dropDownRestaurantDisplayimageLi.append(dropDownRestaurantDisplayimage);
+
+                restaurantUl.append(drowDownRestaurantDisplayLink, dropDownRestaurantDisplayLocationLabelsLi, dropDownRestaurantDisplayRatingLabelLi, dropDownRestaurantDisplayimageLi);
+
+                var dropdownButton = $("<button>").text("Check here for Restaurant Details").addClass("uk-button uk-button-primary").attr("type", "button");
+                var dropDownDiv = $("<div>").attr("uk-dropdown", "");
+
+                dropDownDiv.append(restaurantUl);
+
+                $("#restaurantDisplay").append(dropDownRestaurantNameDisplay, dropdownButton, dropDownDiv);
+                
+                };
+
+                
+                // console.log("location :"+searchResponse.restaurants[0].restaurant.location.address);
+                // console.log("rating :" + searchResponse.restaurants[0].restaurant.user_rating.rating_text);
+                // for (var i = 0; i < 5; i++){
+                   
+                // };
+
+
+
+                // console.log(searchResponse);
+                // console.log(searchResponse.restaurants)
+                // console.log(searchResponse.restaurants[0])
+                // console.log(searchResponse.restaurants[0].restaurant.name)
+                // console.log(searchResponse.restaurants[0].restaurant.url)
+                // console.log(searchResponse.restaurants[0].restaurant.location.address)
+                // console.log(searchResponse.restaurants[0].restaurant.highlights)
             });
     }
     function coinTossOption() {
